bb08e66b7795ac78e60ad5a6f9ee01c1c444a9cd
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-11-10 15:47:29 +1100

    (cs) update class method doco

--- src/Models/Elements/ElementalLinks.php
+++ src/Models/Elements/ElementalLinks.php
@@ -10,41 +10,80 @@ use gorriecoe\Link\Models\Link;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use UncleCheese\DisplayLogic\Forms\Wrapper;
 
+/**
+ * Links element
+ *
+ * @author Mark
+ * @author James
+ */
 class ElementalLinks extends BaseElement
 {
 
+    /**
+     * @inheritdoc
+     */
     private static $icon = 'font-icon-thumbnails';
 
+    /**
+     * @inheritdoc
+     */
     private static $table_name = 'ElementalLinks';
 
+    /**
+     * @inheritdoc
+     */
     private static $title = 'Links list';
 
+    /**
+     * @inheritdoc
+     */
     private static $description = "Display a list of links";
 
+    /**
+     * @inheritdoc
+     */
     private static $singular_name = 'Links Element';
 
+    /**
+     * @inheritdoc
+     */
     private static $plural_name = 'Links Elements';
 
+    /**
+     * @inheritdoc
+     */
     private static $inline_editable = false;
 
+    /**
+     * @var array
+     */
     private static $subtypes = [
         'cards' => 'Cards',
         'feature-tile' => 'Feature tile',
         'link-list' => 'Link list'
     ];
 
+    /**
+     * @var array
+     */
     private static $card_columns = [
         '2' => 'Two',
         '3' => 'Three',
         '4' => 'Four',
     ];
 
+    /**
+     * @var array
+     */
     private static $card_styles = [
         'title' => 'Title only',
         'title-abstract' => 'Title and abstract',
         'title-image-abstract' => 'Title, image, abstract',
     ];
 
+    /**
+     * @inheritdoc
+     */
     private static $db = [
         'HTML' => 'HTMLText',
         'Subtype' => 'Varchar(64)',
@@ -52,36 +91,67 @@ class ElementalLinks extends BaseElement
         'CardStyle' => 'Varchar(64)',
     ];
 
+    /**
+     * @inheritdoc
+     */
     private static $many_many = [
         'ElementLinks' => Link::class
     ];
 
+    /**
+     * @inheritdoc
+     */
     private static $many_many_extraFields = [
         'ElementLinks' => [
             'Sort' => 'Int'
         ]
     ];
 
+    /**
+     * @inheritdoc
+     */
     public function getType()
     {
         return _t(__CLASS__ . '.BlockType', 'Links');
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getCMSFields()
     {
         $fields = parent::getCmsFields();
 
         $fields->removeByName(['ElementLinks']);
 
-        $subType = DropdownField::create('Subtype',_t(__CLASS__ . '.LISTTYPE','List type'),$this->owner->config()->subtypes);
-        $subType->setEmptyString('none');
+        $subType = DropdownField::create(
+            'Subtype',
+            _t(
+                __CLASS__ . '.LISTTYPE',
+               'List type'
+            ),
+            $this->owner->config()->subtypes
+        )->setEmptyString('none');
 
-        $cardColumns = DropdownField::create('CardColumns',_t(__CLASS__ . '.CARDCOLUMNS','Card columns'),$this->owner->config()->card_columns);
-        $cardColumns->setEmptyString('none');
+        $cardColumns = DropdownField::create(
+            'CardColumns',
+            _t(
+                __CLASS__ . '.CARDCOLUMNS',
+               'Card columns'
+            ),
+            $this->owner->config()->card_columns
+        )->setEmptyString('none');
+
         $cardColumns->displayIf('Subtype')->isEqualTo('cards');
 
-        $cardStyle = DropdownField::create('CardStyle',_t(__CLASS__ . '.CARDSTYLE','Card style'),$this->owner->config()->card_styles);
-        $cardStyle->setEmptyString('none');
+        $cardStyle = DropdownField::create(
+            'CardStyle',
+            _t(
+                __CLASS__ . '.CARDSTYLE',
+               'Card style'
+            ),
+            $this->owner->config()->card_styles
+        )->setEmptyString('none');
         $cardStyle->displayIf('Subtype')->isEqualTo('cards');
 
         $fields->addFieldsToTab(
@@ -99,7 +169,10 @@ class ElementalLinks extends BaseElement
                 $cardStyle,
                 LinkField::create(
                     'ElementLinks',
-                    _t(__CLASS__ . '.LINKS','Links'),
+                    _t(
+                        __CLASS__ . '.LINKS',
+                       'Links'
+                    ),
                     $this
                 )->setSortColumn('Sort')
             ]
@@ -108,7 +181,10 @@ class ElementalLinks extends BaseElement
         return $fields;
     }
 
-    public function getColumns()
+    /**
+     * Return column string, by default based off nswds
+     */
+    public function getColumns() : ?string
     {
         $columns = $this->owner->CardColumns;
 
@@ -122,7 +198,7 @@ class ElementalLinks extends BaseElement
             return "nsw-col-sm-6 nsw-col-md-4 nsw-col-lg-3";
         }
 
-        return false;
+        return null;
     }
 
 }
